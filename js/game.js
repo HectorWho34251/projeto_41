7e1de6637787cc35467bc360d4a06059615c09be
HectorWho34251 <122939361+HectorWho34251@users.noreply.github.com>
2023-11-30 12:22:10 -0300

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -130,8 +130,8 @@ class Game{
 
                       if(obstacleGroup.isTouching(players)){
                        // gameState = 0;
-                        gameState = 1;
-                       // gameState = 2;
+                       // gameState = 1;
+                        gameState = 2;
                        // gameState = 3;
                       }
                   }
